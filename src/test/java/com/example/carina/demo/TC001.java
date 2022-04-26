f2a6b0e15c2cba6d98bc641efd309ebdfc26214a
ccristaldo <carlos_cristaldo@hotmail.com>
2022-04-26 10:39:46 -0300

    TC001 - To validate href correctly redirection

--- src/test/java/com/example/carina/demo/TC001.java
+++ src/test/java/com/example/carina/demo/TC001.java
@@ -1,6 +1,20 @@
 package com.example.carina.demo;
 
-public class TC001 {
+import com.example.carina.demo.gui.pages.HomePage;
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.qaprosoft.carina.core.foundation.utils.resources.L10N;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class TC001 implements IAbstractTest {
+
+    @Test
+    public void openHomeTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        homePage.findExtendedWebElement(By.linkText("Big page with many elements")).click();
+        L10N.assertAll();
+    }
 
 
 }
